1e37cb1f8ebba21d4a2e5bce434c1c57529f83fb
unknown <gangsanlee93@gmail.com>
2023-02-13 02:02:26 +0900

    02.13.02:02

--- excel_to_mysql.py
+++ excel_to_mysql.py
@@ -1,81 +1,124 @@
+import string
+import time
+
 import pandas as pd
 import pymysql
+from tqdm import tqdm
 
 
 class ExcelToMySql:
     def __init__(self):
-        self.excel_path = r"C:\Users\Administrator\PycharmProjects\pythonProject\revenue.xlsx"
+        global excel_path, file_name
+        excel_path = r"C:\Users\flussberg\PycharmProjects\dt_from_excel_to_mysql\data/"
+        file_name = "revenue.xlsx"
         self.df = None
         self.conn = None
         self.curs = None
+        self.type_ls = []
+        self.columns = None
+        self.mk_table_sql = None
+        self.insert_sql = None
 
     def load_execl(self):
-        self.df = pd.read_excel(self.excel_path, header=2)
-        self.df = self.df.where((pd.notnull(self.df)), None)
-        print(self.df)
-        print("#"*100)
-        print(self.df.index)
-        print("#"*100)
-        print(self.df.columns)
-        print("#"*100)
-        print(self.df.values)
-        print("#"*100)
-        for i in range(len(self.df.values[0])):
-            print(f"'{self.df.values[0][i]}' type is {type(self.df.values[0][i])}")
+        sheet_name = str(input("사용할 sheet 이름을 입력하시오 (없을 경우 Enter, 첫번째 sheet가 사용됩니다) : "))
+        if sheet_name == "":
+            df = pd.read_excel(excel_path + file_name)
+        else:
+            df = pd.read_excel(excel_path + file_name, sheet_name=sheet_name)
+        print(f"첫 열 줄 \n{df.head(10)}")
+        header = int(input("컬럼명으로 사용할 행의 번호를 입력하시오 : ")) + 1
+        df = pd.read_excel(excel_path + file_name, header=header)
+        print(f"첫 열 줄 \n{df.head(10)}")
+        df = df.where((pd.notnull(df)), None)
+        ls = []
+        for i in range(len(df)):
+            if None in df.values[i]:
+                continue
+            else:
+                for x, y in enumerate(df.values[i]):
+                    ls.append(str(type(y)))
+                break
+        for i in ls:
+            i = i.replace("<class 'pandas._libs.tslibs.timestamps.Timestamp'>", "datetime")
+            i = i.replace("<class 'str'>", "varchar(100)")
+            i = i.replace("<class 'float'>", "float")
+            self.type_ls.append(i)
+        self.df = df
+
+    def make_query(self):
+        columns = []
+        for i in self.df.columns:
+            result = i.translate(str.maketrans('', '', string.punctuation))
+            columns.append(result)
+        sql_ls = []
+        for i, j in enumerate(columns):
+            sql_ls.append(f"{j} {self.type_ls[i]}")
+        mk_table_sql = ', '.join(s for s in sql_ls)
+        mk_table_sql = f'create table if not exists {file_name.split(".")[0]}(' \
+                   f'id int not null auto_increment, ' \
+                   f'{mk_table_sql}, ' \
+                   f'primary key (id))'
+        print(f"테이블 생성 쿼리 : \n{mk_table_sql}")
+        self.mk_table_sql = mk_table_sql
+        insert_sql = f'insert into {file_name.split(".")[0]} ({", ".join(s for s in columns)}) values({("%s," * len(columns)).strip(",")})'
+        print(f"데이터 입력 쿼리 : \n{insert_sql}")
+        self.insert_sql = insert_sql
+        self.columns = columns
 
     def connect_db(self):
-        self.conn = pymysql.connect(host='localhost', user='root', password='root', db='mydb')
-        self.curs = self.conn.cursor()
+        conn = pymysql.connect(host='localhost', user='root', password='root', db='mydb')
+        self.curs = conn.cursor()
+        self.conn = conn
+        print("DB 연결 성공")
 
     def create_table(self):
-        mk_table = 'create table if not exists revenue(' \
-                   'id int not null auto_increment,' \
-                   'cal_date datetime,' \
-                   'cal_day varchar(100),' \
-                   'holiday varchar(100),' \
-                   'dayonoff varchar(100),' \
-                   'product varchar(100),' \
-                   'price float not null,' \
-                   'primary key (id)' \
-                   ')'
-        self.curs.execute(mk_table)
+        self.curs.execute(self.mk_table_sql)
+        print("테이블 생성 완료")
 
     def insert_data(self):
-        sql = 'insert into revenue (cal_date, cal_day, holiday, dayonoff, product, price) values(%s, %s, %s, %s, %s, %s)'
-        for idx in range(len(self.df)):
-            self.curs.execute(sql, tuple(self.df.values[idx]))
+        pbar = tqdm(total=100)
+        for i in range(len(self.df)):
+            self.curs.execute(self.insert_sql, tuple(self.df.values[i]))
+            pbar.update(100/len(self.df))
+        pbar.close()
         self.conn.commit()
+        print("데이터 입력 완료")
 
     def close_db(self):
         self.curs.close()
         self.conn.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     e = ExcelToMySql()
     while True:
-        print('-'*100)
-        menu = int(input("choose menu: "))
-        if menu == 0:
-            print('close')
+        print("-"*100)
+        menu = ["close",
+                "엑셀 파일 데이터 프레임 전환",
+                "쿼리 만들기",
+                "DB 연결",
+                "테이블 생성",
+                "데이터 입력",
+                "DB 연결 종료"]
+        for i, j in enumerate(menu):
+            print(f'{i}.{j}')
+        choice = int(input('메뉴 입력 : '))
+        print(menu[choice])
+        if choice == 0:
             break
-        elif menu == 1:
-            print("엑셀 파일 데이터 프레임 전환")
+        elif choice == 1:
             e.load_execl()
-        elif menu == 2:
-            print("DB 연결")
+        elif choice == 2:
+            e.make_query()
+        elif choice == 3:
             e.connect_db()
-        elif menu == 3:
-            print("테이블 생성")
+        elif choice == 4:
             e.create_table()
-        elif menu == 4:
-            print("데이터 입력 시작")
+        elif choice == 5:
             e.insert_data()
-            print("데이터 입력 완료")
-        elif menu == 5:
-            print("DB 연결 종료")
+        elif choice == 6:
             e.close_db()
         else:
-            print("wrong menu")
+            print(" 잘못된 메뉴 입력 ")
 
 
